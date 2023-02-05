ba4fa68920d83bab5540ad63f941c36ca145891f
IvoLeal72 <ivocleal@gmail.com>
2023-02-05 21:56:18 +0000

    Test

--- item_manager/models.py
+++ item_manager/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MinValueValidator
 from django.db import models, connection
 from django.conf import settings
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.utils.html import format_html
 
 
@@ -26,3 +28,9 @@ class Stock(models.Model):
     item = models.ForeignKey(Item, models.CASCADE, verbose_name=Item._meta.verbose_name)
     storage = models.ForeignKey(Storage, models.CASCADE, verbose_name=Storage._meta.verbose_name)
     quantity = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        unique_together = (('item', 'storage'),)
